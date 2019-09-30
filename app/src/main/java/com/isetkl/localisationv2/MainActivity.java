8eeaaaa44d62ea3f2dbe07b8f2545a76917693bd
AbdAzizFitouri <mohamedabdaziz.fitouri@kelibia.r-iset.tn>
2019-09-30 20:48:55 +0200

    Initial commit

---
+++ app/src/main/java/com/isetkl/localisationv2/MainActivity.java
@@ -0,0 +1,25 @@
+package com.isetkl.localisationv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageView;
+        TextView textView;
+        String langue;
+
+
+        textView = findViewById(R.id.Hello);
+
+
+    }
+}
